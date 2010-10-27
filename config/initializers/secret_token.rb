2128c87cdd4d39a707bcf0ccb6a41fa9bf6cfb83
BRadmin <bradmin@paperdrop.com>
2010-10-27 15:41:45 -0700

    Initial commit, hooray

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5277ac22edaafafba7d81b5b8c7c7814c1acbd4bddc85a80f708ca9fdbec11b27272a2633e07912c163341223740967f81817b851336d999f9b521a69726e15'
